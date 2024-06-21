02d72a020a9d96f0ed1f7a25074431aa381ba513
Anouar Andichi <73336825+AnouarAndichi@users.noreply.github.com>
2024-06-21 22:37:16 +0200

    Update xtream-ui_installer.py

--- xtream-ui_installer.py
+++ xtream-ui_installer.py
@@ -116,7 +116,7 @@ def encrypt(rHost="127.0.0.1", rUsername="user_iptvpro", rPassword="", rDatabase
     data = '{{"host":"{}","db_user":"{}","db_pass":"{}","db_name":"{}","server_id":"{}","db_port":"{}"}}'.format(
         rHost, rUsername, rPassword, rDatabase, rServerID, rPort).encode('utf-8')
     
-    encrypted_data = ''.join(chr(data[i] ^ key[i % len(key)]) for i in range(len(data))).encode('utf-8')
+    encrypted_data = bytes(chr(data[i] ^ key[i % len(key)]) for i in range(len(data)))
     encoded_data = base64.b64encode(encrypted_data).decode('utf-8')
     
     with open('/home/xtreamcodes/iptv_xtream_codes/config', 'wb') as rf:
